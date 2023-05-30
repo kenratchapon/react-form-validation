9dab0b3c2c9c1c5f01ac0216e51b66ed800312d0
Ken Ratchapon <ratchapon.pk@outlook.com>
2023-05-30 17:49:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FormComponent from './components/FormComponent';
+
+function App() {
+  return (
+    <div>
+		<FormComponent/>
+    </div>
+  );
+}
+
+export default App;
